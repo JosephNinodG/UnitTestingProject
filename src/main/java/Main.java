c3db8d544ee851ec512782567fd587518af53cd9
JosephNinodG <61060613+JosephNinodG@users.noreply.github.com>
2020-04-20 14:58:16 +0100

    Initial commit
    Intialising repo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,13 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Main myMain = new Main();
 
-        System.out.println(myMain.addStuff(5,9));
-
-    }
-
-    public int addStuff(int a, int b){
-        return a+b;
     }
 }
--- src/test/java/Testing.java
+++ src/test/java/Testing.java
@@ -1,15 +1,4 @@
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
-
 public class Testing {
 
-    @Test
-    public void testEquals(){
-        Main myMain = new Main();
-        int a = 9;
-        int b = 5;
-        assertEquals(14, myMain.addStuff(9,5));
-    }
+
 }
