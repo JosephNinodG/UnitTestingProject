cde0c0f150e332394bfcffab73e7704260a399a7
JosephNinodG <61060613+JosephNinodG@users.noreply.github.com>
2020-04-21 12:04:32 +0100

    Practice Unit Test
    Simple unit tests that make sure that a multiplication or division by 0, returns 0.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,32 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Main mainClass = new Main();
 
+        int a = 0;
+
+        for (int b = 0; b < 10; b++) {
+            System.out.println(mainClass.multiply(a, b));
+            System.out.println(mainClass.addition(a, b));
+            System.out.println(mainClass.subtract(a, b));
+            System.out.println(mainClass.division(a, b));
+        }
+
+    }
+
+    public int multiply(int a, int b){
+        return a*b;
+    }
+
+    public int addition(int a, int b){
+        return a+b;
+    }
+
+    public int subtract(int a, int b){
+        return a-b;
+    }
+
+    public int division(int a, int b){
+        return a/b;
     }
 }
--- src/test/java/Testing.java
+++ src/test/java/Testing.java
@@ -1,4 +1,27 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class Testing {
 
+    @Test
+    public void multiplicationOfZeroIntegerShouldReturnZero() {
+        Main multiplyTester = new Main(); //Main is tested
+
+        assertEquals(0, multiplyTester.multiply(10,0),"10 x 0 must be 0");
+        assertEquals(0, multiplyTester.multiply(0,10),"0 x 10 must be 0");
+        assertEquals(0, multiplyTester.multiply(0,0),"0 x 0 must be 0");
+
+
+    }
+
+    @Test
+    public void divisionOfZeroIntegerShouldReturnZero() {
+        Main divisionTester = new Main(); //Main is tested
+
+        assertEquals(0, divisionTester.multiply(10,0),"10 / 0 must be 0");
+        assertEquals(0, divisionTester.multiply(0,10),"0 / 10 must be 0");
+        assertEquals(0, divisionTester.multiply(0,0),"0 / 0 must be 0");
+
+    }
 
 }
